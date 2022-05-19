6e53afa0272e97c58eca15ce7fa5dac38de53f53
Ulugbek <utn1002@gmail.com>
2022-05-19 00:05:49 -0700

    docker

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
-const host = process.env.PORT;
+const host = process.env.HOST;
 
 
 app.get("/test/", (req, res) => {
